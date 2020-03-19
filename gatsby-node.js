31f359d1db9469bee680f828cfe8f601d4bd6d75
Josh Mu <mu@joshmu.com>
2020-03-19 14:12:53 +1000

    added potential reference to another script which successfully uses multiple collections for createPages in gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,63 @@
 const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
+// graphql function doesn't throw an error so we have to check to check for the result.errors to throw manually
+const wrapper = promise =>
+  promise.then(result => {
+    if (result.errors) {
+      throw result.errors
+    }
+    return result
+  })
+
+// ? single graphql query for multiple collections using aliases?
+// https://github.com/LekoArts/portfolio/blob/master/gatsby-node.js
+// https://github.com/LekoArts/portfolio/blob/master/src/gatsby/gatsbyNodeGraphQL.js
+const gatsbyNodeGraphQL = `
+  blog: allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+    edges {
+      node {
+        timeToRead
+        parent {
+          ... on File {
+            relativeDirectory
+          }
+        }
+        fields {
+          slug
+        }
+        frontmatter {
+          title
+        }
+      }
+    }
+  }
+  projects: allPrismicProjekt(sort: { fields: [data___date], order: DESC }) {
+    edges {
+      node {
+        fields {
+          slug
+        }
+        lang
+        data {
+          title {
+            text
+          }
+          cover {
+            localFile {
+              childImageSharp {
+                resize(width: 600) {
+                  src
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
 
