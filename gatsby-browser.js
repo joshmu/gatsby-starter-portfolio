fa2f7424b062d7cb230ed1f3a8336eca8ddb6df2
Josh Mu <mu@joshmu.com>
2020-03-22 12:15:09 +1000

    typeface Playfair Display

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,7 @@
 // custom typefaces
-import "typeface-montserrat"
-import "typeface-merriweather"
+// import "typeface-montserrat"
+// import "typeface-merriweather"
+import "typeface-playfair-display"
 
 // import "prismjs/themes/prism.css"
 import "prismjs/themes/prism-okaidia.css"
