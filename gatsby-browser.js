eb381419e188ebe63cb6d56927c05e7b6b757b16
Josh Mu <mu@joshmu.com>
2020-03-22 17:16:29 +1000

    config update for netlifycms

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,7 +3,7 @@
 // import "typeface-merriweather"
 import "typeface-playfair-display"
 
-// import "prismjs/themes/prism.css"
-import "prismjs/themes/prism-okaidia.css"
+import "prismjs/themes/prism.css"
+// import "prismjs/themes/prism-okaidia.css"
 
 import "./src/styles/global.scss"
