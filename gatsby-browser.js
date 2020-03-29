55e45478bd9138d1b2483be1bcd2d2af90794fd3
Josh Mu <mu@joshmu.com>
2020-03-29 14:59:15 +1000

    fixed font-family requirements

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,8 @@
 // custom typefaces
 // import "typeface-montserrat"
 // import "typeface-merriweather"
-// import "typeface-playfair-display"
+import "typeface-playfair-display"
+import "typeface-roboto"
 
 import "prismjs/themes/prism.css"
 // import "prismjs/themes/prism-okaidia.css"
